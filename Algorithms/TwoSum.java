e5a4ade3eb4e36cba2d19cf4f362c801766620d8
Thomas <rthomasanders@protonmail.ch>
2023-08-29 10:35:03 +0200

    Create AddTwoNumbers.java

---
+++ Algorithms/TwoSum.java
@@ -0,0 +1,29 @@
+/**
+ * 1. Two Sum
+ * 
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * You can return the answer in any order.
+ * 
+ * @author Thomas
+ * @since 2023-08-29
+ */
+
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+
+        int i;
+
+        for (i = 0; i < nums.length; i++) {
+            int j;
+            for (j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    int[] result = {i, j};
+                    return result;
+                }
+            }
+        }
+        return new int[0];
+        
+    }
+} 
