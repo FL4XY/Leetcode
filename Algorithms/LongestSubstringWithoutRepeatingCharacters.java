18b0fd6ffebfc4f2611be7398b448d102d10cb8f
Thomas <rthomasanders@protonmail.ch>
2023-08-29 10:39:10 +0200

    Create LongestSubstringWithoutRepeatingCharacters.java

---
+++ Algorithms/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,15 @@
+/**
+ * 3. Longest Substring Without Repeating Characters
+ * 
+ * Given a string s, find the length of the longest substring without repeating characters.
+ * 
+ * @author Thomas
+ * @since 2023-08-29
+ */
+public class LongestSubstringWithoutRepeatingCharacters {
+
+    public int lengthOfLongestSubstring(String s) {
+        
+    }
+    
+}
